bff187c9604c99a83077ce10ad815aa518b6d7fe
Masataka Hirano <gooogle4747@gmail.com>
2018-05-12 19:58:40 +0900

    Update RSpec to use equality matchers rather than raise_error matchers

--- spec/models/currency_converter_spec.rb
+++ spec/models/currency_converter_spec.rb
@@ -5,32 +5,32 @@ require 'pry'
 require './boot.rb'
 
 RSpec.describe CurrencyConverter do
-  describe '#validate!' do
-    subject { converter.validate! }
+  describe '#errors' do
+    subject { converter.tap(&:valid?).errors.to_h }
 
     context 'with valid attributes' do
       let(:converter) { build(:currency_converter) }
-      it { expect { subject }.not_to raise_error }
+      it { is_expected.to be_empty }
     end
 
     context 'with date which is in the future' do
       let(:converter) { build(:currency_converter, date: Date.tomorrow) }
-      it { expect { subject }.to raise_error(ActiveModel::ValidationError, /Date can't be in the future/) }
+      it { is_expected.to include(date: "can't be in the future") }
     end
 
     context 'with amount_in_currency_from which is 0' do
       let(:converter) { build(:currency_converter, amount_in_currency_from: 0) }
-      it { expect { subject }.to raise_error(ActiveModel::ValidationError, /Amount in currency from must be greater than 0/) }
+      it { is_expected.to include(amount_in_currency_from: 'must be greater than 0') }
     end
 
     context 'with currency_from which is not in the currencies list' do
       let(:converter) { build(:currency_converter, currency_from: 'FoO') }
-      it { expect { subject }.to raise_error(ActiveModel::ValidationError, /Currency from accepts only three charactors of upper case/) }
+      it { is_expected.to include(currency_from: 'accepts only three charactors of upper case') }
     end
 
     context 'with currency_to which is not in the currencies list' do
       let(:converter) { build(:currency_converter, currency_to: 'FOO') }
-      it { expect { subject }.to raise_error(ActiveModel::ValidationError, /Currency to must be valid one/) }
+      it { is_expected.to include(currency_to: 'must be valid one') }
     end
   end
 
@@ -38,8 +38,8 @@ RSpec.describe CurrencyConverter do
     subject { converter.convert! }
 
     context 'the amount currency from 10,000 JPY to AUD at 2017-02-22' do
-      let(:converter) { build(:currency_converter) }
-      it { is_expected.to eq 114.56 }
+      let(:converter) { build(:currency_converter, date: '2017-02-22', amount_in_currency_from: 10_000, currency_from: 'JPY', currency_to: 'AUD') }
+      it { is_expected.to eq(114.56) }
     end
   end
 end
--- spec/models/open_exchange_rates_client_spec.rb
+++ spec/models/open_exchange_rates_client_spec.rb
@@ -5,17 +5,17 @@ require 'pry'
 require './boot.rb'
 
 RSpec.describe OpenExchangeRatesClient do
-  describe '#validate!' do
-    subject { client.validate! }
+  describe '#errors' do
+    subject { client.tap(&:valid?).errors.to_h }
 
     context 'with valid parameters' do
       let(:client) { build(:open_exchange_rates_client) }
-      it { expect { subject }.not_to raise_error }
+      it { is_expected.to be_empty }
     end
 
     context 'without app_id' do
       let(:client) { build(:open_exchange_rates_client, app_id: '') }
-      it { expect { subject }.to raise_error(ActiveModel::ValidationError, /App can't be blank/) }
+      it { is_expected.to include(app_id: "can't be blank") }
     end
   end
 end
