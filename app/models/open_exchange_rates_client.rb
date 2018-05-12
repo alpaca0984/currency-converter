675de12c177515317d9294cc62b4efdc92ddb11e
Masataka Hirano <gooogle4747@gmail.com>
2018-05-12 10:25:04 +0900

    Update OpenExchangeRatesClient to use frozen strings effectively

--- app/models/open_exchange_rates_client.rb
+++ app/models/open_exchange_rates_client.rb
@@ -11,13 +11,13 @@ class OpenExchangeRatesClient
   def base_url
     URI::HTTPS.build(
       host: 'openexchangerates.org',
-      path: '/api'
+      path: '/api'.dup # to append further strings later
     )
   end
 
   def fetch_currencies
     currencies_url = base_url.tap do |url|
-      url.path += '/currencies.json'
+      url.path << '/currencies.json'
     end
     Net::HTTP.get(currencies_url)
   end
@@ -26,7 +26,7 @@ class OpenExchangeRatesClient
   def fetch_historical_for(date:)
     validate!
     historical_url = base_url.tap do |url|
-      url.path += "/historical/#{date.to_date.strftime('%F')}.json"
+      url.path << "/historical/#{date.to_date.strftime('%F')}.json"
       url.query = "app_id=#{app_id}"
     end
     Net::HTTP.get(historical_url)
