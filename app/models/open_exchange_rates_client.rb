7f4c2ab711f80bed64a9bbc60e534dcd9cd76591
Masataka Hirano <gooogle4747@gmail.com>
2018-05-12 10:00:46 +0900

    Add frozen_string_literal the top of files

--- app/models/open_exchange_rates_client.rb
+++ app/models/open_exchange_rates_client.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class OpenExchangeRatesClient
   include ActiveModel::Validations
   include ActiveModel::Attributes
@@ -15,7 +17,7 @@ class OpenExchangeRatesClient
 
   def fetch_currencies
     currencies_url = base_url.tap do |url|
-      url.path << '/currencies.json'
+      url.path += '/currencies.json'
     end
     Net::HTTP.get(currencies_url)
   end
@@ -24,7 +26,7 @@ class OpenExchangeRatesClient
   def fetch_historical_for(date:)
     validate!
     historical_url = base_url.tap do |url|
-      url.path << "/historical/#{date.to_date.strftime('%F')}.json"
+      url.path += "/historical/#{date.to_date.strftime('%F')}.json"
       url.query = "app_id=#{app_id}"
     end
     Net::HTTP.get(historical_url)
